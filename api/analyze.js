93bf3f7c1ab2e39eaf18e758bb260686b95fceb2
UNCLEROD35 <rlstafford58@gmail.com>
2026-03-06 17:14:59 -0500

    Update analyze.js
    changed gemini flash from 2.5 to 1.5

--- api/analyze.js
+++ api/analyze.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
         }
     };
     try {
-        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${apiKey}`;
+        const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-preview-09-2025:generateContent?key=${apiKey}`;
         const response = await fetch(url, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
